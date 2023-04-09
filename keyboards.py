74be5f0715293e8f25d6cbd3c7dbccef80ce32e8
Daniil <azbukin2003@gmail.com>
2023-04-10 02:03:05 +0300

    Добавил календарь

--- keyboards.py
+++ keyboards.py
@@ -1,23 +1,25 @@
 from aiogram.types import ReplyKeyboardMarkup,ReplyKeyboardRemove, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram_calendar import SimpleCalendar
 
 def get_inline_keyboard(id)->InlineKeyboardMarkup:
     ikb=InlineKeyboardMarkup()
     if (id=='main_menu'):
         ikb = InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton('Сегодня', callback_data='today_btn'),InlineKeyboardButton('Завтра', callback_data='tommorow_btn')],
-            [InlineKeyboardButton('Ввести дату', callback_data='current_date_btn')],
-            [InlineKeyboardButton('Эта неделя', callback_data='week_btn')],
-            [InlineKeyboardButton('Изменить номер группы', callback_data='change_group_num_btn')]
+            [InlineKeyboardButton('Сегодня', callback_data='today_main_btn'),InlineKeyboardButton('Завтра', callback_data='tommorow_main_btn')],
+            [InlineKeyboardButton('Ввести дату', callback_data='current_date_main_btn')],
+            [InlineKeyboardButton('Выбор даты через календарь', callback_data='calendar_main_btn')],
+            [InlineKeyboardButton('Эта неделя', callback_data='week_main_btn')],
+            [InlineKeyboardButton('Изменить номер группы', callback_data='change_group_num_main_btn')]
         ])
     if id == 'timetable':
         ikb=InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton('Следующий день', callback_data='next_day_btn')],
-            [InlineKeyboardButton('Назад', callback_data='back_btn')]
+            [InlineKeyboardButton('Следующий день', callback_data='next_day_tmtb_btn')],
+            [InlineKeyboardButton('Назад', callback_data='back_tmtb_btn')]
         ])
     if id == 'back_from_enddate':
         ikb=InlineKeyboardMarkup(inline_keyboard=[
             [InlineKeyboardButton("Отменить",
-                                  callback_data='back_enddate_btn')]
+                                  callback_data='back_end_btn')]
         ])
     return ikb
 
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from aiogram import Dispatcher, Bot, executor,types
 from aiogram.dispatcher import FSMContext
-from aiogram.types import ReplyKeyboardMarkup,ReplyKeyboardRemove, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup,ReplyKeyboardRemove, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from aiogram.utils.callback_data import CallbackData
 from globals import TOKEN_API, sampleGroup, sampleDate
 from datetime import datetime, timedelta
 from keyboards import get_inline_keyboard
+from aiogram_calendar import SimpleCalendar, simple_cal_callback
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import StatesGroup, State
@@ -46,13 +47,13 @@ async def get_group(message: types.Message, state: FSMContext):
             await bot.send_message(message.from_user.id,
                                    text="Получить расписание на (выберите одно из указанных ниже)",
                                    reply_markup=get_inline_keyboard('main_menu'))
+            await state.finish()
         else:
             await bot.send_message(chat_id=message.from_user.id,
                                    text="Не смогли найти данную группу. Попробуйте еще раз.")
     else:
         await bot.send_message(chat_id=message.from_user.id,
                                text="Не правильный формат группы, попробуйте еще раз.")
-    await state.finish()
 
 #Вывод расписания по конкретной дате
 @dp.message_handler(state=GroupStates.date)
@@ -80,44 +81,66 @@ async def main_menu_message(callback):
     await callback.message.edit_text(text="Получить расписание на (выберите одно из указанных ниже)",
                                      reply_markup=get_inline_keyboard('main_menu'))
 
-# #Inline kb обработчик
-@dp.callback_query_handler(lambda callback_querry: callback_querry.data.endswith('btn'), state='*')
-async def ik_cb_handler(callback: types.CallbackQuery, state: FSMContext):
-    print('flag')
+#Обработчик клавиатуры в главном меню
+@dp.callback_query_handler(lambda callback_querry: callback_querry.data.endswith('main_btn'), state='*')
+async def ik_cb_main_handler(callback: types.CallbackQuery, state: FSMContext):
     #Выдача расписания на сегодня
-    if callback.data=='today_btn':
+    if callback.data=='today_main_btn':
         await callback.message.edit_text(text=current_day_timetable(datetime.today()),
                                    reply_markup=get_inline_keyboard("timetable"))
     #Выдача расписания на завтра
-    elif callback.data=='tommorow_btn':
+    elif callback.data=='tommorow_main_btn':
         one_day=timedelta(days=1)
         await callback.message.edit_text(text=current_day_timetable(datetime.today()+one_day),
                                          reply_markup=get_inline_keyboard("timetable"))
     #Выдача расписания на определенный день
-    elif callback.data == 'current_date_btn':
+    elif callback.data == 'current_date_main_btn':
         await GroupStates.date.set()
         await callback.message.edit_text(text='Введите дату в формате 00.00.0000',
                                          reply_markup=get_inline_keyboard("back_from_enddate"))
     #Выдача расписания на неделю
-    elif callback.data == 'week_btn':
+    elif callback.data == 'week_main_btn':
         a=1
     #Смена номера группы
-    elif callback.data == 'change_group_num_btn':
+    elif callback.data == 'change_group_num_main_btn':
         await GroupStates.group.set()
         await callback.message.edit_text(text="Введите номер группы.\n"
                                               "Формат БСБО-10-21",
                                          reply_markup=get_inline_keyboard('back_from_enddate'))
-    #Обработка кнопки следующего дня в расписании
-    elif callback.data == 'next_day_btn':
-        a=1
-    #Обработка кнопки назад в расписании
-    elif callback.data == 'back_btn':
+
+    #Пробую календарь на вкус
+    if callback.data=='calendar_main_btn':
+        await callback.message.edit_text(text='Выберите дату:',
+                                         reply_markup=await SimpleCalendar().start_calendar())
+
+@dp.callback_query_handler(simple_cal_callback.filter())
+async def process_simple_calendar(callback_query: CallbackQuery, callback_data: dict):
+    selected, date = await SimpleCalendar().process_selection(callback_query, callback_data)
+    if selected:
+        await callback_query.message.edit_text(text=current_day_timetable(date),
+            reply_markup=get_inline_keyboard('timetable')
+        )
+
+#Обработчик клавиатуры расписания
+@dp.callback_query_handler(lambda callback_querry: callback_querry.data.endswith('tmtb_btn'), state='*')
+async def ik_cb_tmtb_handler(callback: types.CallbackQuery, state: FSMContext):
+    # Обработка кнопки следующего дня в расписании
+    if callback.data == 'next_day_tmtb_btn':
+        a = 1
+    # Обработка кнопки назад в расписании
+    elif callback.data == 'back_tmtb_btn':
         await main_menu_message(callback)
-    #Обработка кнопки отменить при вводе даты пользователем
-    elif callback.data == 'back_enddate_btn':
+
+#обработчик клавиатуры Отмены
+@dp.callback_query_handler(lambda callback_querry: callback_querry.data.endswith('end_btn'), state='*')
+async def ik_cb_end_handler(callback: types.CallbackQuery, state: FSMContext):
+    #Обработка кнопки отменить при вводе даты пользователем и ввода группы
+    cur_state = await state.get_state()
+    if callback.data == 'back_end_btn' and cur_state is not None:
         await main_menu_message(callback)
         await state.finish()
-
+    elif callback.data == 'back_end_btn':
+        await callback.message.edit_reply_markup(reply_markup=None)
 
 #Получение расписания по дате!!!!!!!!!!!!!!!!!!
 def current_day_timetable(date)->str:
