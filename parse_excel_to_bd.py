4f616d80d3d9303ae46149d0e8341cc588e14800
Artyomka334 <kachalov2003artyom@mail.ru>
2023-04-10 21:57:00 +0300

    DB CONNECTION

--- parse_excel_to_bd.py
+++ parse_excel_to_bd.py
@@ -1,10 +1,9 @@
 import sqlite3
 
+def group_to_bd(user_id, group):
+    sqlite_connection = sqlite3.connect('Timetable_DB.db')
+    cursor = sqlite_connection.cursor()
+    insert_user_id = f"INSERT INTO User (user_tg_id, group_num) VALUES ({user_id}, {group})"
+    cursor.execute(insert_user_id)
 
-sqlite_connection = sqlite3.connect('Timetable_DB.db')
-cursor = sqlite_connection.cursor()
 
-sqlite_select_query = ""
-record = cursor.fetchall()
-print("Версия базы данных SQLite: ", record)
-cursor.close()
